ff19da0fe3e14435592b4a9428020972eab74f3a
2works-Akash <akash@2works.io>
2025-03-31 20:57:34 +0530

    List-> List concatingation, repeating same list and slicing list etc operations

---
+++ List/basicOperationsOnList.py
@@ -0,0 +1,9 @@
+a= ["mathematics", "statistics", 10, 20, "java", "python"]
+b=[123, "addy"]
+
+print(a, b) #Print Both lists
+print(a[0]) # Print zeroth index value of list a
+print(a[1:4]) # Print list of a from 1st index to 3 by slicing
+print(a[2:]) #print List from 2nd index by slicing
+print(b*2) # Repeating the same element of list b 2 times.
+print(a+b) # Concatenating List a and b together
